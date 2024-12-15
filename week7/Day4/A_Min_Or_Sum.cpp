05f436e80c0ff251dc5df3176ff08de6c3bad8f3
Rakib <rakibhasan75089@gmail.com>
2024-12-15 23:31:29 +0600

    Day7 solved 1 problem

--- week7/Day4/A_Min_Or_Sum.cpp
+++ week7/Day4/A_Min_Or_Sum.cpp
@@ -1,21 +1,22 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-
-int main() {
+void solve(){
+    int n;cin>>n;
+    vector<int> v(n);
+    int result = 0;
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+        result |= v[i];
+    }
+    cout<<result<<endl;
+}
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        int result = 0;
-        for (int i = 0; i < n; ++i) {
-            int x;
-            cin >> x;
-            result |= x;
-        }
-        cout << result << endl;
+    int t;cin>>t;
+    while(t--){
+        solve();
     }
     return 0;
 }
