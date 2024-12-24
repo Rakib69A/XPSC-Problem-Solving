f1d12228e0bee8e22ea1c75d87d06f761fb22594
Rakib <rakibhasan75089@gmail.com>
2024-12-24 23:07:42 +0600

    Day7 solved 3 problem

--- week7/Day7/B_Binary_Colouring.cpp
+++ week7/Day7/B_Binary_Colouring.cpp
@@ -1,48 +1,30 @@
-#include "bits/stdc++.h"
-
-#define all(a) a.begin(), a.end()
-#define pb push_back
-typedef long long ll;
+#include <bits/stdc++.h>
 using namespace std;
-mt19937 rnd(std::chrono::high_resolution_clock::now().time_since_epoch().count());
- 
-/// Actual code starts here
-const int N = 100005;
- 
-void solve() {
-    ll x;
-    cin >> x;
-    vector<int> res(31, 0);
-    for (int i = 0; i < 30; i++) {
-        if (1ll & (x >> i)) {
-            if (res[i]) {
-                res[i + 1] = 1;
-                res[i] = 0;
-            } else if (i > 0) {
-                if (res[i - 1] == 1) {
-                    res[i + 1] = 1;
-                    res[i - 1] = -1;
-                } else {
-                    res[i] = 1;
-                }
-            } else if (i == 0) {
-                res[i] = 1;
-            }
+void solve(){
+    int n;cin>>n;
+    int len = log2(n);
+    cout<<31<<endl;
+    vector<int> v;
+    for(int i=0;i<=30;i++){
+        if(n & (1<<i)){
+            v.push_back(1);
+        }
+        else{
+            v.push_back(0);
         }
     }
-    cout << 31 << '\n';
-    for (int i = 0; i <= 30; i++) {
-        cout << res[i] << ' ';
-    }
-    cout << '\n';
+    for(int val:v)
+        cout<<val<<" ";
+    cout<<endl;
 }
- 
-signed main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    int tt = 1;
-    cin >> tt;
-    while (tt--)
+    int t;cin>>t;
+    while(t--){
         solve();
+    }
+    return 0;
 }
