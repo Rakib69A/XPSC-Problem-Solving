b5215afbd75932b9c86c42d13c6593e503358efd
Rakib <rakibhasan75089@gmail.com>
2024-12-29 23:47:16 +0600

    Day5 solved 1 problem

--- week8/Day3/B_Deja_Vu.cpp
+++ week8/Day3/B_Deja_Vu.cpp
@@ -1,46 +1,29 @@
 #include <bits/stdc++.h>
-#define ll long long
 using namespace std;
-const ll inf    = 1e9;
 void solve(){
-    int n, q;
-        cin >> n >> q;
-        vector<int> a(n), b(q);
-        for (auto& i : a) {
-            cin >> i;
-        }
-        for (auto& i : b) {
-            cin >> i;
-        }
-        vector<int> smallerIdx(35, inf);
-        for (int i = 0; i < q; i++) {
-            if (smallerIdx[b[i]] == inf) {
-                smallerIdx[b[i]] = i;
+    int n,q;
+    cin>>n>>q;
+    vector<int> a(n);
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+    int query;
+    vector<int> b;
+    for(int i=0;i<q;i++){
+        cin>>query;
+        if(b.empty() || b.back()>query)
+            b.push_back(query);
+    }
+    for(int i=0;i<n;i++){
+        for(auto it:b){
+            if(a[i]%(1<<it)==0){
+                it=it-1;
+                a[i]+=(1<<it);
             }
         }
-        for (int i = 0; i < n; i++) {
-            int firstIdx = inf;
-            for (int j = 1; j <= 30; j++) {
-                if (a[i] % (1 << j) == 0) {
-                    firstIdx = min(firstIdx, smallerIdx[j]);
-                }
-            }
-            while (firstIdx < q) {
-                a[i] += (1 << (b[firstIdx] - 1));
-                int x = b[firstIdx] - 1;
-                int xx = inf;
-                while(x >= 0) {
-                    if(smallerIdx[x] != inf && smallerIdx[x] > firstIdx) {
-                        xx = min(xx, smallerIdx[x]);
-                    }
-                    x--;
-                }
-                firstIdx = xx;
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            cout << a[i] << " \n"[i == n - 1];
-        }
+    }
+    for(auto it:a)
+        cout<<it<<" ";
+    cout<<endl;
 }
 int main()
 {
